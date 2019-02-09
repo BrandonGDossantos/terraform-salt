2a1ddc50cff872c7458750104fb8a89913c050e0
BrandonGDossantos <bxd7887@rit.edu>
2019-02-08 19:11:40 -0500

    Added region part atleast

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@
 #     access_key = "${var.access_key}"
 #     secret_key = "${var.secret_key}"
 # }
-# provider "aws" {
-#     region = "${var.region}"
-#     access_key = "${var.access_key}"
-#     secret_key = "${var.secret_key}"
-# }
+provider "aws" {
+    region = "us-east-1"
+    # access_key = "${var.access_key}"
+    # secret_key = "${var.secret_key}"
+}
 terraform {
     backend "s3" {
         bucket = "leix.terraform-salt.s3"
