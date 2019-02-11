ca6ce6a5a52e9b61da661440ae804a150fddcc29
BrandonGDossantos <bxd7887@rit.edu>
2019-02-10 21:15:59 -0500

    fixes

--- instances/saltminion.tf
+++ instances/saltminion.tf
@@ -26,26 +26,3 @@ resource "aws_instance" "saltminion" {
         }
     }
 }
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh_sg"
-  description = "Allow SSH inbound connections"
-  vpc_id = "${aws_vpc.default.id}"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "allow_ssh_sg"
-  }
-}
--- instances/vpc.tf
+++ instances/vpc.tf
@@ -40,76 +40,26 @@ resource "aws_route_table_association" "default_us_east_1a_public" {
     subnet_id = "${aws_subnet.public.id}"
     route_table_id = "${aws_route_table.default_us_east_1b_public.id}"
 }
-/*
-  Private Subnet
-*/
-# resource "aws_subnet" "us-east-1b-private" {
-#     vpc_id = "${aws_vpc.default.id}"
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_sg"
+  description = "Allow SSH inbound connections"
+  vpc_id = "${aws_vpc.default.id}"
 
-#     cidr_block = "${var.private_subnet_cidr}"
-#     availability_zone = "us-east-1b"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#     tags {
-#         Name = "Private Subnet"
-#     }
-# }
-# resource "aws_security_group" "nat" {
-#     name = "vpc_nat"
-#     description = "Allow traffic to pass from the private subnet to the internet"
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 
-#     ingress {
-#         from_port = 80
-#         to_port = 80
-#         protocol = "tcp"
-#         cidr_blocks = ["${var.private_subnet_cidr}"]
-#     }
-#     ingress {
-#         from_port = 443
-#         to_port = 443
-#         protocol = "tcp"
-#         cidr_blocks = ["${var.private_subnet_cidr}"]
-#     }
-#     ingress {
-#         from_port = 22
-#         to_port = 22
-#         protocol = "tcp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-#     ingress {
-#         from_port = -1
-#         to_port = -1
-#         protocol = "icmp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-
-#     egress {
-#         from_port = 80
-#         to_port = 80
-#         protocol = "tcp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-#     egress {
-#         from_port = 443
-#         to_port = 443
-#         protocol = "tcp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-#     egress {
-#         from_port = 22
-#         to_port = 22
-#         protocol = "tcp"
-#         cidr_blocks = ["${var.vpc_cidr}"]
-#     }
-#     egress {
-#         from_port = -1
-#         to_port = -1
-#         protocol = "icmp"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
-
-#     vpc_id = "${aws_vpc.default.id}"
-
-#     tags {
-#         Name = "NATSG"
-#     }
-# }
+  tags {
+    Name = "allow_ssh_sg"
+  }
+}
